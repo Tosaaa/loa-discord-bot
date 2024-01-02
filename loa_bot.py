2ab8f8f244aaac64b7437e5ef0c00517773b75f0
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2024-01-02 14:53:54 +0000

    Added loa_bot.py

---
+++ loa_bot.py
@@ -0,0 +1,28 @@
+import asyncio
+import discord
+
+client = discord.Client(intents=discord.Intents.all())
+token = "" # available on raspberry pi
+
+@client.event
+async def on_ready():
+    print("Logged in as ")
+    print(client.user.name)
+    print(client.user.id)
+    print("=============")
+    await client.change_presence(activity=discord.Game(name="반가워요", type=1))
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return None
+
+    id = message.author.id
+    channel = message.channel
+
+    if message.content.startswith("!로아봇"):
+        await message.channel.send("왜불러")
+    else:
+        await message.channel.send("\""+message.content+"\"라고 말함.")
+
+client.run(token)
