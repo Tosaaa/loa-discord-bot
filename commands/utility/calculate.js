abd5b5ab901dacaa960591571fa2d6e9955f9cc6
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2024-01-09 14:55:55 +0000

    Add calculate.js

---
+++ commands/utility/calculate.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('쌀')
+        .setDescription('쌀산기')
+        .addIntegerOption(option => 
+            option.setName('몇인')
+                .setDescription('몇인 레이드?')
+                .setRequired(true)
+                .addChoices(
+                    {name: "4인", value: 4},
+                    {name: "8인", value: 8},
+                    {name: "16인", value: 16}
+                ))
+        .addIntegerOption(option => 
+            option.setName('경매가')
+                .setDescription('경매 최저가')
+                .setRequired(true)),
+    async execute(interaction) {
+        const divider = interaction.options.getInteger('몇인');
+        const auctionValue = interaction.options.getInteger('경매가');
+        
+        const equalValue = (auctionValue * 0.95) * (divider-1) / divider;
+        const preemptionValue = equalValue / 1.1;
+
+        const res = `공평: ${equalValue.toFixed(2)}, \n선점: ${preemptionValue.toFixed(2)}`;
+        await interaction.reply(res);
+    },
+}
