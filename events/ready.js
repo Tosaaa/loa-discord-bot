b6980c3078fda536f62326929e2f3de52f22d52a
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2024-01-31 09:39:38 +0000

    Add loabot avatar

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,11 @@ const { Events } = require('discord.js');
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
 
+        await client.user.setAvatar('DB/loabot_icon.png');
         // displaying client under maintanence
-        // client.user.setPresence({ activities: [{ name: '수리' }], status: 'dnd' });
+        client.user.setPresence({ activities: [{ name: '수리' }], status: 'dnd' });
 	},
 }
