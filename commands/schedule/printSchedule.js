d02dbf847c3209efddb6f9b1bdc72bd2e08db8f3
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2024-02-01 14:43:00 +0000

    Minor change

--- commands/schedule/printSchedule.js
+++ commands/schedule/printSchedule.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute(interaction) {
         const client = interaction.client;
 
-        let result = `**[이번 주 레이드 일정]**\n`;
+        let result = `**[이번 주 일정 현황]**\n`;
         let embedList = [];
         
         embedList.push(this.createEmbedBySchedule(client.schedule));
@@ -22,7 +22,7 @@ module.exports = {
     createEmbedBySchedule(schedule) {
         const embed = {}
         embed.color = 0xf0dc7a;
-        embed.title = `이번 주 레이드 일정`;
+        embed.title = `이번 주 일정`;
         embed.fields = [];
         for (const scheduleKey of Object.keys(schedule)) {
             //TODO: 딜, 폿 구분해서 출력하기 (클래스 DB 만들어야 할 듯)
